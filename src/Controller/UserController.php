9643f9cdd12c72ed84e75963c4f6c919cd838fbb
Nicolas Gihoul <ngihoul@hotmail.com>
2024-01-19 14:39:49 +0100

    Correctin route order

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -44,6 +44,34 @@ class UserController extends AbstractController
         ]);
     }
 
+    #[Route('/profile/update', name: 'app_profile_update')]
+    #[IsGranted('ROLE_USER')]
+    public function update(Request $request, ProfilePictureManager $profilePictureManager): Response
+    {
+        $user = $this->getUser();
+
+        $form = $this->createForm(UserType::class, $user);
+
+        // Handle form
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user = $form->getData();
+
+            $profilePictureManager->handleProfilePicture($form, $user);
+
+            $this->entityManager->persist($user);
+            $this->entityManager->flush();
+
+            return $this->redirectToRoute('app_profile', ['userId' => $user->getId()]);
+        }
+
+        return $this->render('profile/update.html.twig', [
+            'form' => $form->createView(),
+            'image' => $user->getProfilePicture()
+        ]);
+    }
+
     #[Route('/profile/{userId}', name: 'app_profile')]
     public function profileUser(Request $request, UserRepository $userRepository): Response
     {
@@ -72,33 +100,5 @@ class UserController extends AbstractController
             $this->addFlash('error', 'Le profil demandé n\'a pas été trouvé.');
             return $this->redirectToRoute('app_home');
         }
-    }
-
-    #[Route('/profile/update', name: 'app_profile_update')]
-    #[IsGranted('ROLE_USER')]
-    public function update(Request $request, ProfilePictureManager $profilePictureManager): Response
-    {
-        $user = $this->getUser();
-
-        $form = $this->createForm(UserType::class, $user);
-
-        // Handle form
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $user = $form->getData();
-
-            $profilePictureManager->handleProfilePicture($form, $user);
-
-            $this->entityManager->persist($user);
-            $this->entityManager->flush();
-
-            return $this->redirectToRoute('app_profile', ['userId' => $user->getId()]);
-        }
-
-        return $this->render('profile/update.html.twig', [
-            'form' => $form->createView(),
-            'image' => $user->getProfilePicture()
-        ]);
     }
 }
