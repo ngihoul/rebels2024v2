33cf0e03be6681ffd8e36227ac409f853d0db042
Nicolas Gihoul <ngihoul@hotmail.com>
2024-01-23 10:43:12 +0100

    Translate Login page

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -3,20 +3,21 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 class LoginController extends AbstractController
 {
-    #[Route('/login', name: 'app_login')]
-    public function index(AuthenticationUtils $authenticationUtils): Response
+    #[Route('/login', name: 'app_login', locale: 'fr')]
+    public function index(AuthenticationUtils $authenticationUtils, TranslatorInterface $translator, Request $request): Response
     {
         // Denied access if user is already logged in.
         if ($this->getUser()) {
-            $this->addFlash('error', 'tu es déjà connecté.');
-            return $this->redirectToRoute('app_home');
+            $this->addFlash('error', $translator->trans('error.already_logged'));
+            return $this->redirectToRoute('app_home', ['_locale' => $request->getLocale()]);
         }
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
