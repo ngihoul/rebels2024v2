da06702ef8f9a4e4b6d331d2cb70c284ef41e1a6
Nicolas Gihoul <ngihoul@hotmail.com>
2024-01-31 11:56:12 +0100

    Factorising AdminLicenseController

--- src/Controller/AdminLicenseController.php
+++ src/Controller/AdminLicenseController.php
@@ -8,6 +8,7 @@ use App\Repository\LicenseRepository;
 use App\Service\EmailManager;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\EntityNotFoundException;
+use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -41,55 +42,52 @@ class AdminLicenseController extends AbstractController
     #[IsGranted('ROLE_ADMIN')]
     public function validate(Request $request, EmailManager $emailManager, TranslatorInterface $translator): Response
     {
-        $licenseId = $request->get('licenseId');
-
-        // Find license in DB
         try {
+            $licenseId = $request->get('licenseId');
             $license = $this->licenseRepository->find($licenseId);
             if (!$license) {
                 throw new EntityNotFoundException($translator->trans('error.license.not_found'));
             }
-        } catch (EntityNotFoundException $e) {
+            $form = $this->createForm(ValidateLicenseType::class);
+
+            $form->handleRequest($request);
+
+            if ($form->isSubmitted() && $form->isValid()) {
+                // Check which button was clicked 
+                if ($form->get('approval')->isClicked()) {
+                    // Change status to DOC_VALIDATED
+                    $license->setStatus(License::DOC_VALIDATED);
+
+                    // Add price to License
+                    $license->setPrice($form->get('price')->getData());
+
+                    // Send a mail to player informing him his license has been validated
+                    $emailManager->sendEmail($license->getUser()->getEmail(), 'Licence validée', 'license_approved');
+                } elseif ($form->get('refusal')->isClicked()) {
+                    // Change status to ON_DEMAND
+                    $license->setStatus(License::ON_DEMAND);
+
+                    // Add comment to License
+                    $license->setComment($form->get('comment')->getData());
+
+                    // Send a mail to player informing him his license has been refused
+                    $emailManager->sendEmail($license->getUser()->getEmail(), 'Licence refusée', 'license_refused', ['reason' => $license->getComment()]);
+                }
+
+                $this->entityManager->persist($license);
+                $this->entityManager->flush();
+
+                // Redirect to list of license to validate
+                return $this->redirectToRoute('admin_license_to_validate');
+            }
+
+            return $this->render('admin/license/validate.html.twig', [
+                'form' => $form->createView(),
+                'license' => $license
+            ]);
+        } catch (Exception $e) {
             $this->addFlash('error', $e->getMessage());
             return $this->redirectToRoute('admin_license_to_validate');
         }
-
-        $form = $this->createForm(ValidateLicenseType::class);
-
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            // Check which button was clicked 
-            if ($form->get('approval')->isClicked()) {
-                // Change status to DOC_VALIDATED
-                $license->setStatus(License::DOC_VALIDATED);
-
-                // Add price to License
-                $license->setPrice($form->get('price')->getData());
-
-                // Send a mail to player informing him his license has been validated
-                $emailManager->sendEmail($license->getUser()->getEmail(), 'Licence validée', 'license_approved');
-            } elseif ($form->get('refusal')->isClicked()) {
-                // Change status to ON_DEMAND
-                $license->setStatus(License::ON_DEMAND);
-
-                // Add comment to License
-                $license->setComment($form->get('comment')->getData());
-
-                // Send a mail to player informing him his license has been refused
-                $emailManager->sendEmail($license->getUser()->getEmail(), 'Licence refusée', 'license_refused', ['reason' => $license->getComment()]);
-            }
-
-            $this->entityManager->persist($license);
-            $this->entityManager->flush();
-
-            // Redirect to list of license to validate
-            return $this->redirectToRoute('admin_license_to_validate');
-        }
-
-        return $this->render('admin/license/validate.html.twig', [
-            'form' => $form->createView(),
-            'license' => $license
-        ]);
     }
 }
