5624bc2066670dce423ea896b50b1b06e38c1716
Nicolas Gihoul <ngihoul@hotmail.com>
2024-02-07 13:36:26 +0100

    Translating - FileUploader Service

--- src/Service/FileUploader.php
+++ src/Service/FileUploader.php
@@ -2,20 +2,24 @@
 
 namespace App\Service;
 
+use Exception;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\Mime\MimeTypesInterface;
 use Symfony\Component\String\Slugger\SluggerInterface;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 class FileUploader
 {
     private SluggerInterface $slugger;
+    private TranslatorInterface $translator;
     private ParameterBagInterface $container;
     private MimeTypesInterface $mimeTypes;
 
-    public function __construct(SluggerInterface $slugger, ParameterBagInterface $container, MimeTypesInterface $mimeTypes)
+    public function __construct(SluggerInterface $slugger, TranslatorInterface $translator, ParameterBagInterface $container, MimeTypesInterface $mimeTypes)
     {
         $this->slugger = $slugger;
+        $this->translator = $translator;
         $this->container = $container;
         $this->mimeTypes = $mimeTypes;
     }
@@ -31,7 +35,7 @@ class FileUploader
         $fileMimeType = $this->mimeTypes->guessMimeType($image->getPathname());
 
         if (!in_array($fileMimeType, $allowedImageMimeTypes)) {
-            throw new \Exception('Le fichier n\'est pas une image valide.');
+            throw new Exception($this->translator->trans('error.file.not_valid'));
         }
 
         $originalFileName = pathinfo($image->getClientOriginalName(), PATHINFO_FILENAME);
