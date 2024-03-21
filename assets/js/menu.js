204019c8284f30f821f0d834171d3a2290342ae3
Nicolas Gihoul <ngihoul@hotmail.com>
2024-03-21 13:44:19 +0100

    Cleanup JS folders & files

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,26 +1,28 @@
-if (document.getElementById("hamburger-icon")) {
-  const hamburgerIcon = document.getElementById("hamburger-icon");
-  const closeIcon = document.getElementById("close-icon");
-  const menu = document.querySelector(".menu");
+document.addEventListener("DOMContentLoaded", () => {
+  if (document.getElementById("hamburger-icon")) {
+    const hamburgerIcon = document.getElementById("hamburger-icon");
+    const closeIcon = document.getElementById("close-icon");
+    const menu = document.querySelector(".menu");
 
-  hamburgerIcon.addEventListener("click", function () {
-    menu.classList.add("menu-open");
-    menu.classList.remove("menu-closed");
-  });
+    hamburgerIcon.addEventListener("click", function () {
+      menu.classList.add("menu-open");
+      menu.classList.remove("menu-closed");
+    });
 
-  closeIcon.addEventListener("click", function () {
-    menu.classList.remove("menu-open");
-    menu.classList.add("menu-closed");
-  });
-}
+    closeIcon.addEventListener("click", function () {
+      menu.classList.remove("menu-open");
+      menu.classList.add("menu-closed");
+    });
+  }
 
-const container = document.querySelector(".container");
-const nav = document.querySelector("nav");
+  const container = document.querySelector(".container");
+  const nav = document.querySelector("nav");
 
-if (nav) {
-  container.classList.add("nav-open");
-  container.classList.remove("nav-closed");
-} else {
-  container.classList.add("nav-closed");
-  container.classList.remove("nav-open");
-}
+  if (nav) {
+    container.classList.add("nav-open");
+    container.classList.remove("nav-closed");
+  } else {
+    container.classList.add("nav-closed");
+    container.classList.remove("nav-open");
+  }
+});
