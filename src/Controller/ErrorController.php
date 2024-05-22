b1fefed44bbf4c50b9266c4cc39598687a316435
Nicolas Gihoul <ngihoul@hotmail.com>
2024-05-22 16:17:19 +0200

    Translating page not found error

--- src/Controller/ErrorController.php
+++ src/Controller/ErrorController.php
@@ -4,12 +4,13 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 class ErrorController extends AbstractController
 {
-    public function show(): Response
+    public function show(TranslatorInterface $translator): Response
     {
-        $this->addFlash('error', 'Cette page n\'existe pas');
+        $this->addFlash('error', $translator->trans('error.page.not_found'));
         return $this->redirectToRoute('app_home');
     }
 }
