1b2d3f2f74e1c001d6177806d707f7e8327c6ca6
Nicolas Gihoul <ngihoul@hotmail.com>
2024-05-28 19:37:56 +0200

    Installing CKEditor

--- config/bundles.php
+++ config/bundles.php
@@ -17,4 +17,5 @@ return [
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
+    FOS\CKEditorBundle\FOSCKEditorBundle::class => ['all' => true],
 ];
