7ef15541379e5bae815882b5e9329938471e0e1f
Nicolas Gihoul <ngihoul@hotmail.com>
2024-06-12 19:56:59 +0200

    Cleanup use statements

--- src/Form/ValidateLicenseType.php
+++ src/Form/ValidateLicenseType.php
@@ -6,11 +6,8 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Validator\Constraints\NotBlank;
-use Symfony\Component\Validator\Constraints\Type;
 
 class ValidateLicenseType extends AbstractType
 {
