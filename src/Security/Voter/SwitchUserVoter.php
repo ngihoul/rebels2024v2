780871eeffbed6ad2988402a7316e04fbd23014e
Nicolas Gihoul <ngihoul@hotmail.com>
2024-10-01 11:46:30 +0200

    Deleting debug logging

--- src/Security/Voter/SwitchUserVoter.php
+++ src/Security/Voter/SwitchUserVoter.php
@@ -3,19 +3,11 @@
 namespace App\Security\Voter;
 
 use App\Entity\User;
-use Psr\Log\LoggerInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Authorization\Voter\Voter;
 
 class SwitchUserVoter extends Voter
 {
-    private $logger;
-
-    public function __construct(LoggerInterface $logger)
-    {
-        $this->logger = $logger;
-    }
-
     protected function supports(string $attribute, mixed $subject): bool
     {
         return $attribute === 'SWITCH' && $subject instanceof User;
@@ -26,7 +18,6 @@ class SwitchUserVoter extends Voter
         $parent = $token->getUser();
 
         if (!$parent instanceof User) {
-            $this->logger->info('L\'utilisateur courant n\'est pas une instance de User.');
             return false;
         }
 
@@ -36,23 +27,17 @@ class SwitchUserVoter extends Voter
     private function canSwitch(User $parent, User $child): bool
     {
         if (!in_array('ROLE_PARENT', $parent->getRoles())) {
-            $this->logger->info('L\'utilisateur courant n\'a pas le rôle ROLE_PARENT.');
             return false;
         }
 
         $children = $parent->getChildren()->toArray();
-        $this->logger->info('Enfants du parent:', ['children' => $children]);
 
         foreach ($children as $c) {
-            $this->logger->info('Enfant fetché depuis le parent:' . $c->getId() . $c->getFirstname());
-            $this->logger->info('Enfant passé à canSwitch:' . $child->getId() . $child->getFirstname());
             if ($c->getId() === $child->getId()) {
-                $this->logger->info('L\'enfant est trouvé dans la liste des enfants du parent.');
                 return true;
             }
         }
 
-        $this->logger->info('L\'enfant n\'est pas dans la liste des enfants du parent.');
         return false;
     }
 }
