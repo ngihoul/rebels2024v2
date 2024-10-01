5c5f908a98cc0123107c2b8738dd899fded58328
Nicolas Gihoul <ngihoul@hotmail.com>
2024-10-01 18:34:30 +0200

    Styling profile to list children

--- src/Service/ProfilePictureManager.php
+++ src/Service/ProfilePictureManager.php
@@ -15,16 +15,37 @@ class ProfilePictureManager
 
     public function handleProfilePicture($form, $user): void
     {
-        $picture = $form->get('profile_picture')->getData();
+        $picture = $form->get('profile_picture')->getData() ?? $user->getProfilePicture() ?? null;
 
         if ($picture) {
-            try {
-                $pictureFileName = $this->fileUploader->save($picture, 'pictures_directory');
-                $user->setProfilePicture($pictureFileName);
-            } catch (FileException $e) {
-                // Handle exception or log it
-                // For example: $this->logger->error('Error saving profile picture: ' . $e->getMessage());
-            }
+            $this->saveProfilePicture($picture, $user);
+        } else {
+            $this->setDefaultProfilePicture($user);
+        }
+    }
+
+    private function saveProfilePicture($picture, $user): void
+    {
+        try {
+            $pictureFileName = $this->fileUploader->save($picture, 'pictures_directory');
+            $user->setProfilePicture($pictureFileName);
+        } catch (FileException $e) {
+            // Handle exception or log it
+            // For example: $this->logger->error('Error saving profile picture: ' . $e->getMessage());
+        }
+    }
+
+    private function setDefaultProfilePicture($user): void
+    {
+        $userGender = $user->getGender();
+        $userAge = $user->getAge();
+
+        if ($userAge < 16) {
+            $user->setProfilePicture('default/default-academy.png');
+        } else if ($userGender == "F") {
+            $user->setProfilePicture('default/default-softball.png');
+        } else if ($userGender == "M") {
+            $user->setProfilePicture('default/default-baseball.png');
         }
     }
 }
