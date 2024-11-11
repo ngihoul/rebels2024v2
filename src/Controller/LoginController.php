65efa1183528ce7ac9cdd1bff4d62bea7af9c56b
Nicolas Gihoul <ngihoul@hotmail.com>
2024-11-11 14:34:52 +0100

    Cleaning session when reloading login page

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,6 +13,13 @@ use Symfony\Contracts\Translation\TranslatorInterface;
 
 class LoginController extends AbstractController
 {
+    private SessionInterface $session;
+
+    public function __construct(RequestStack $requestStack,)
+    {
+        $this->session = $requestStack->getSession();
+    }
+
     #[Route('/login', name: 'app_login')]
     public function index(AuthenticationUtils $authenticationUtils, TranslatorInterface $translator, Request $request, SessionInterface $session): Response
     {
@@ -20,6 +28,11 @@ class LoginController extends AbstractController
             $this->addFlash('error', $translator->trans('error.already_logged'));
             return $this->redirectToRoute('app_home', ['_locale' => $request->getLocale()]);
         }
+
+        // Clean session
+        $this->session->clear('step');
+        $this->session->clear('user_choice');
+
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
         // last username entered by the user
