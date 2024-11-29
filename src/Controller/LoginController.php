4306bda11a86a3435180cb3c3a3316d6be2f9eef
Nicolas Gihoul <ngihoul@hotmail.com>
2024-11-29 10:49:32 +0100

    Correcting session call

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -4,46 +4,32 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 use Symfony\Contracts\Translation\TranslatorInterface;
 
 class LoginController extends AbstractController
 {
-    private SessionInterface $session;
-
-    public function __construct(RequestStack $requestStack,)
-    {
-        $this->session = $requestStack->getSession();
-    }
-
     #[Route('/login', name: 'app_login')]
-    public function index(AuthenticationUtils $authenticationUtils, TranslatorInterface $translator, Request $request, SessionInterface $session): Response
+    public function index(AuthenticationUtils $authenticationUtils, TranslatorInterface $translator, Request $request): Response
     {
+
         $user = $this->getUser();
 
-        // Denied access if user is already logged in.
         if ($user) {
             $this->addFlash('error', $translator->trans('error.already_logged'));
             return $this->redirectToRoute('app_home', ['_locale' => $request->getLocale()]);
         }
 
-        // Clean session
-        $this->session->remove('step');
-        $this->session->remove('user_choice');
+        $session = $request->getSession();
 
-        // get the login error if there is one
+        $session->remove('step');
+        $session->remove('user_choice');
+
         $error = $authenticationUtils->getLastAuthenticationError();
-        // last username entered by the user
-        $lastUsername = $authenticationUtils->getLastUsername();
 
-        // Assuming the user is authenticated here, store the user ID in the session
-        if ($user) {
-            $session->set('activeUser', $user->getId());
-        }
+        $lastUsername = $authenticationUtils->getLastUsername();
 
         return $this->render('login/index.html.twig', [
             'last_username' => $lastUsername,
